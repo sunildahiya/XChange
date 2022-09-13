0f104a5c7313837bfe8932d5c7419a6b53e34fb8
Sunil Saini <sainisunil302@gmail.com>
2022-09-13 16:00:30 +0530

    Configure ObjectMapper used to parse streaming messages to use JavaTimeModule for parsing timestamp

--- xchange-stream-service-netty/src/main/java/info/bitrich/xchangestream/service/netty/StreamingObjectMapperHelper.java
+++ xchange-stream-service-netty/src/main/java/info/bitrich/xchangestream/service/netty/StreamingObjectMapperHelper.java
@@ -3,6 +3,7 @@ package info.bitrich.xchangestream.service.netty;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
  * This class should be merged with ObjectMapperHelper from XStream..
@@ -15,6 +16,8 @@ public class StreamingObjectMapperHelper {
 
   static {
     objectMapper = new ObjectMapper();
+    // Fix: If we decide to use long for all timestamp values, then this is not required
+    objectMapper.registerModule(new JavaTimeModule());
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     objectMapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
     objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
